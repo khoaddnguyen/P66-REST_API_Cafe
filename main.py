e1a1182286f70b731d24d52a7c69be350f53d405
kwa_laa <khoaddnguyen@gmail.com>
2023-09-28 15:20:28 -0500

    convert table to dictionary type

--- main.py
+++ main.py
@@ -26,6 +26,15 @@ class Cafe(db.Model):
     can_take_calls = db.Column(db.Boolean, nullable=False)
     coffee_price = db.Column(db.String(250), nullable=True)
 
+    def to_dict(self):
+        # dictionary = {}
+        # for column in self.__table__.columns:
+        #     dictionary[column.name] = getattr(self, column.name)
+        # return dictionary
+
+        # or use dictionary comprehension
+        return {column.name: getattr(self, column.name) for column in self.__table__.column}
+
 
 with app.app_context():
     db.create_all()
@@ -42,19 +51,7 @@ def get_random():
         result = db.session.execute(db.select(Cafe))
         all_cafes = result.scalars().all()
         random_cafe = random.choice(all_cafes)
-        return jsonify(cafe={
-            "id": random_cafe.id,
-            "name": random_cafe.name,
-            "map_url": random_cafe.map_url,
-            "img_url": random_cafe.img_url,
-            "location": random_cafe.location,
-            "seats": random_cafe.seats,
-            "has_toilet": random_cafe.has_toilet,
-            "has_wifi": random_cafe.has_wifi,
-            "has_sockets": random_cafe.has_sockets,
-            "can_take_calls": random_cafe.can_take_calls,
-            "coffee_price": random_cafe.coffee_price,
-        })
+        return jsonify(cafe=random_cafe.to_dict())
     
 
 ## HTTP GET - Read Record
