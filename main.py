a724c87c536ea7b7e7fbae2817b3c7f5079068b2
kwa_laa <khoaddnguyen@gmail.com>
2023-09-29 15:02:09 -0500

    add the /add route to main, send a post request on Postman to enter a new cafe location

--- main.py
+++ main.py
@@ -74,6 +74,28 @@ def search_cafe():
 
 ## HTTP POST - Create Record
 
+
+# Notes on sending a POST request on POSTMAN:
+# url: http://127.0.0.1:5000/add
+# navigate to Body, select "x-www-form-urlencoded", then enter all params
+@app.route("/add", methods=["POST"])
+def post_new_cafe():
+    new_cafe = Cafe(
+        name=request.form.get("name"),
+        map_url=request.form.get("map_url"),
+        img_url=request.form.get("img_url"),
+        location=request.form.get("loc"),
+        has_sockets=bool(request.form.get("sockets")),
+        has_toilet=bool(request.form.get("toilet")),
+        has_wifi=bool(request.form.get("wifi")),
+        can_take_calls=bool(request.form.get("calls")),
+        seats=request.form.get("seats"),
+        coffee_price=request.form.get("coffee_price"),
+    )
+    db.session.add(new_cafe)
+    db.session.commit()
+    return jsonify(response={"success": "Successfully added the new cafe."})
+
 ## HTTP PUT/PATCH - Update Record
 
 ## HTTP DELETE - Delete Record
